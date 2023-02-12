9560621d2f30ce6189fbc918879e3379972121ed
Dmitriy Romanyuk <dmitriy.romanyuk1@gmail.com>
2023-02-12 23:43:51 +0300

    DZ

--- task2_DZ/Program.cs
+++ task2_DZ/Program.cs
@@ -9,11 +9,15 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 == number2*number2)
+if (number1 > number2)
 {
-    Console.Write("Первое число является квадратом второго");
+    Console.Write($"max = {number1}, min = {number2} ");
+}
+else if (number1 < number2)
+{
+    Console.Write($"max = {number2}, min = {number1} ");
 }
 else
 {
-    Console.Write("Первое число НЕ является квадратом второго");
+    Console.Write($"Числа равны. {number1} = {number2}");
 }
